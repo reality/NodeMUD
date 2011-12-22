b326fae119c22fe75071c46ca51e40e9d1f1cf0f
Luke Slater <reality@turtle.nourishedcloud.com>
2011-12-22 16:05:47 +0000

    sandbox

--- run.js
+++ run.js
@@ -14,7 +14,7 @@ var sandboxGen = function(nmud, user) {
 var testUser = {
     'name': 'reality',
     'commands': {
-        'test': vm.createScript('say(\'fish\');')
+        'test': "say('fish');"
     },
     'socket': null
 };
@@ -33,7 +33,7 @@ var NodeMUD = function() {
         socket.on('data', function(input) {
             var sandbox = sandboxGen(this, socket.user);
             var commandName = chomp(input.toString()).split(' ')[0];
-            console.log(commandName);
+
             if(socket.user.commands.hasOwnProperty(commandName)) {
                 vm.runInNewContext(socket.user.commands[commandName], sandbox);
             } else {
