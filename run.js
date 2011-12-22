cee50eda7d02c135510667fbcf07caaae71fdaeb
Luke Slater <reality@turtle.nourishedcloud.com>
2011-12-22 00:53:12 +0000

    broadcast

--- run.js
+++ run.js
@@ -1,7 +1,16 @@
 var net = require('net');
 
+var sockets = [];
+
 var server = net.createServer(function(socket) {
+    sockets.push(socket);
     socket.write("Welcome to NodeMud, bitches.\r\n");
- });
+});
+
+server.on('data', function(data) {
+    for(index in sockets) {
+        sockets[index].write(data);
+    }
+});
 
 server.listen(1337, "64.30.136.166");
