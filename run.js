9ff2d2490a6732a7c4238a829bb0ebfac7920235
Luke Slater <reality@turtle.nourishedcloud.com>
2011-12-22 13:46:18 +0000

    sandbox

--- run.js
+++ run.js
@@ -1,18 +1,35 @@
 var net = require('net');
+var vm = require('vm');
 
-var connections = [];
-
-var server = net.createServer(function(socket) {
-    connections.push(socket);
-    socket.write('Welcome to NodeMUD, bitches.\r\n');
-
-    socket.on('data', function(data) {
-        for(index in connections) {
-            if(connections[index] != socket) {
-                connections[index].write(data);
-            }
+var sandbox = function(nmud) {
+    return {
+        'say': function(text) {
+            nmud.broadcast(text);        
         }
-    });
-});
+    };
+};
 
-server.listen(1337, "64.30.136.166");
+var NodeMUD = function() {
+    this.sandbox = sandbox(this);
+    this.connections = [];
+
+    this.server = net.createServer(function(socket) {
+        connections.push(socket);
+        socket.write('Welcome to NodeMUD, bitches.\r\n');
+
+        socket.on('data', function(data) {
+            vm.runInNewContext(data, sandbox);
+        });
+    }.bind(this));
+
+    server.listen(1337, "64.30.136.166");
+}
+NodeMUD.prototype.broadcast = function(text) {
+    for(index in this.connections) {
+        if(this.connections[index] != socket) {
+            this.connections[index].write(data);
+        }
+    }
+}
+
+new NodeMUD();
