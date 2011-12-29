6d67c62b6e3fe8c976ed3a2ea2b03f2b2dcdaca6
Luke Slater <reality@turtle.nourishedcloud.com>
2011-12-29 16:27:52 +0000

    capitalise and full stop that shit

--- globals/say.js
+++ globals/say.js
@@ -2,11 +2,17 @@ var text = p.slice(1).join(' ');
 
 var verb = 'says';
 var modifier = text.substr(-1);
-if(modifier === '?') {
-    verb = 'asks';
-} else if(modifier === '!') {
-    verb = 'exclaims';
+if(modifier === '!' || modifier === '?') || modifier === '.') {
+    if(modifier === '?') {
+        verb = 'asks';
+    } else if(modifier === '!') {
+        verb = 'exclaims';
+    }
+} else {
+    text += '.';
 }
+
+text = text.charAt(0).toUpperCase() + text.slice(1);
 
 echo('You ' + verb.slice(0, -1) + ' "' + text + '"');
 oecho(username + ' ' + verb + ' "' + text + '"');
